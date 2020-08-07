3f660876c91c7cd0773de7799b7d241ac4cb890d
jinseo-jang <jinseo.jang@gmail.com>
2020-08-07 14:18:57 +0900

    Updates

---
+++ precache-manifest.fc4fa7fa5a18eda4fe780808149ccbd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db26538bfd3ebf4dec5d54dab6c10d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8344638852d0f5da174e",
+    "url": "/static/css/main.2d1b8f8f.chunk.css"
+  },
+  {
+    "revision": "da91fca76777d8e65fc5",
+    "url": "/static/js/2.e2494b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e2494b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8344638852d0f5da174e",
+    "url": "/static/js/main.ca1fb954.chunk.js"
+  },
+  {
+    "revision": "bd7f4f55b55e38cea2f1",
+    "url": "/static/js/runtime-main.58002af7.js"
+  }
+]);
